34458bab5b85d715beb171e77f82a1ced45174c4
arnaud-gaetan <o.arnaud.gaetan@gmail.com>
2016-04-22 20:36:21 +0200

    Taking off the declaration of the component into the main seeing that it already been declared in Zcomponents from which it will called

--- rtl/tx.vhd
+++ rtl/tx.vhd
@@ -23,43 +23,11 @@ end tx;
 
 architecture a_tx of tx is
 
-	component bit_to_symbol is
-		port(
-			from_mac: in  std_logic;
-			symbol:   out std_logic_vector(Nbits_symb-1 downto 0);
-			reset: 	  in  std_logic;
-			Fb:       in  std_logic;
-			Fs:       in  std_logic;
-			clk:	  in  std_logic
-		);
-	end component bit_to_symbol;
-
-	component symbol_to_chip is
-		port(
-			symbol:   in  std_logic_vector(Nbits_symb-1 downto 0);
-			ich0: 	  out std_logic;
-			qch0: 	  out std_logic;
-			reset: 	  in  std_logic;
-			Fs:       in  std_logic;
-			Fc:       in  std_logic;
-			clk:	  in  std_logic
-		);
-	end component symbol_to_chip;
-
-	component shaper is
-		port(
-			ich0: 	  in  std_logic;
-			qch0: 	  in  std_logic;
-			ich1:     out std_logic_vector(Nbits_dac-1 downto 0);
-			qch1:     out std_logic_vector(Nbits_dac-1 downto 0);
-			reset: 	  in  std_logic;
-			Fc:       in  std_logic;
-			Fe:       in  std_logic;
-			clk:	  in  std_logic
-		);
-	end component shaper;
+	
 begin
-
+U1 : bit_to_symbol  PORT MAP();
+U2 : symbol_to_chip PORT MAP();
+U3 : shaper PORT MAP();
 
 end a_tx;
 
