f3b3b12e12bf95e733a32dc503e17d39fd4eb7a5
arnaud-gaetan <o.arnaud.gaetan@gmail.com>
2016-04-24 14:34:45 +0200

    This file contains basic definition of bit_to_symbol function

---
+++ rtl/bit_to_symbol.vhd
@@ -0,0 +1,21 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_signed.all;
+use work.pack.all;
+
+
+entity bit_to_symbol is
+		port(
+			from_mac: in  std_logic;
+			symbol:   out std_logic_vector(Nbits_symb-1 downto 0);
+			reset: 	  in  std_logic;
+			Fb:       in  std_logic;
+			Fs:       in  std_logic;
+			clk:	  in  std_logic
+		);
+end bit_to_symbol;
+
+architecture a_bit_to_symbol of bit_to_symbol is
+
+
+end a_bit_to_symbol;
