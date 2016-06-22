a544544dd686e9353497391189abc26c0e83144a
arnaud-gaetan <o.arnaud.gaetan@gmail.com>
2016-06-22 18:24:44 +0200

    Correction brought to the declaration of ech as array to a vector and and change of ich0-1 to ich0_1 as a better declaration. Wrong values was also affected to each sample and has been corrected too.

--- rtl/shaper.vhd
+++ rtl/shaper.vhd
@@ -17,55 +17,51 @@ entity shaper is
 end shaper;
 
 architecture a_shaper of shaper is
-	type ech is array (4 downto 0) is std_logic;
-	type reg is array (4 downto 0) is ech;
-	signal ich0-1,qch0-1: reg;
+	type ech is std_logic_vector (4 downto 0);
+	type reg is array (3 downto 0) of ech;
+	signal ich0_1,qch0_1: reg;
 
 	begin
 		ech_symbol:process(clk)
 			begin
 				if clk'event and clk='1'
 					if reset='1' then 
-						ich0-1(4 downto 0) <= (others => 0);
-						qch0-1(4 downto 0) <= (others => 0);
+						ich0_1(3 downto 0) <= (others => 0);
+						qch0_1(3 downto 0) <= (others => 0);
 					elsif Fc='1' then
 						case ich0 is 
-							when '0' =>
-								ich0-1(0) <= std_logic_vector(to_signed(0, 5)),
-								ich0-1(1) <= std_logic_vector(to_signed(0, 5)),
-								ich0-1(2) <= std_logic_vector(to_signed(15, 5)),
-								ich0-1(3) <= std_logic_vector(to_signed(31, 5)),
-								ich0-1(4) <= std_logic_vector(to_signed(15, 5));
 							when '1' =>
-								ich0-1(0) <= std_logic_vector(to_signed(1, 5)),
-								ich0-1(1) <= std_logic_vector(to_signed(0, 5)),
-								ich0-1(2) <= std_logic_vector(to_signed(-15, 5)),
-								ich0-1(3) <= std_logic_vector(to_signed(-31, 5)),
-								ich0-1(4) <= std_logic_vector(to_signed(-15, 5));
+								ich0_1(0) <= std_logic_vector(to_signed(0, 5)),
+								ich0_1(1) <= std_logic_vector(to_signed(8, 5)),
+								ich0_1(2) <= std_logic_vector(to_signed(15, 5)),
+								ich0_1(3) <= std_logic_vector(to_signed(8, 5));
+							when '0' =>
+								ich0_1(0) <= std_logic_vector(to_signed(0, 5)),
+								ich0_1(1) <= std_logic_vector(to_signed(-8, 5)),
+								ich0_1(2) <= std_logic_vector(to_signed(-15, 5)),
+								ich0_1(3) <= std_logic_vector(to_signed(-8, 5));
 							when others null
 						end case;
 						
 						case qch0 is 
-							when '0' =>
-								qch0-1(0) <= std_logic_vector(to_signed(0, 5)),
-								qch0-1(1) <= std_logic_vector(to_signed(0, 5)),
-								qch0-1(2) <= std_logic_vector(to_signed(15, 5)),
-								qch0-1(3) <= std_logic_vector(to_signed(31, 5)),
-								qch0-1(4) <= std_logic_vector(to_signed(15, 5));
 							when '1' =>
-								qch0-1(0) <= std_logic_vector(to_signed(1, 5)),
-								qch0-1(1) <= std_logic_vector(to_signed(0, 5)),
-								qch0-1(2) <= std_logic_vector(to_signed(-15, 5)),
-								qch0-1(3) <= std_logic_vector(to_signed(-31, 5)),
-								qch0-1(4) <= std_logic_vector(to_signed(-15, 5));
+								qch0_1(0) <= std_logic_vector(to_signed(0, 5)),
+								qch0_1(1) <= std_logic_vector(to_signed(8, 5)),
+								qch0_1(2) <= std_logic_vector(to_signed(15, 5)),
+								qch0_1(3) <= std_logic_vector(to_signed(8, 5));
+							when '0' =>
+								qch0_1(0) <= std_logic_vector(to_signed(0, 5)),
+								qch0_1(1) <= std_logic_vector(to_signed(-8, 5)),
+								qch0_1(2) <= std_logic_vector(to_signed(-15, 5)),
+								qch0_1(3) <= std_logic_vector(to_signed(-8, 5));
 							when others null
 						end case;
 					end if;
 					if Fe='1' then
-						ich1 <= ich0-1(0);
-						qch1 <= qch0-1(0);
-						ich0-1(3 downto 0) <= ich0_1(4 downto 1);
-						ich0-1(3 downto 0) <= ich0_1(4 downto 1);
+						ich1 <= ich0_1(0);
+						qch1 <= qch0_1(0);
+						ich0_1(2 downto 0) <= ich0_1(3 downto 1);
+						qch0_1(2 downto 0) <= qch0_1(3 downto 1);
 					end if;
 						
 								
