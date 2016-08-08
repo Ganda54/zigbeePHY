405c9a8b7e9143ce5c55e7b00ad0b52b1533f3dc
Ganda <ganda@nestwave.com>
2016-08-08 11:29:16 +0200

    Edited architecture so as to get a less exhaustive description. Now uses a dynamic index for accessing array of chips.
    Solved issue with "reg" being static by turning it into a variable.
    
    Reads both channel I and Q from the same register reg.

--- rtl/symbol_to_chip.vhd
+++ rtl/symbol_to_chip.vhd
@@ -1,6 +1,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use IEEE.STD_LOGIC_arith.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
 use work.pack.all;
 
 
@@ -17,8 +19,7 @@ entity symbol_to_chip is
 end symbol_to_chip;
 
 architecture a_symbol_to_chip of symbol_to_chip is
-	subtype chip_sec is std_logic_vector (31 downto 0); 
-	type mem      is array (15 downto 0) of chip_sec;
+	type mem      is array (0 to 15) of std_logic_vector (31 downto 0);
 	constant code : mem:= ("11011001110000110101001000101110",
 										   "11101101100111000011010100100010",
 										   "00101110110110011100001101010010",
@@ -35,44 +36,21 @@ architecture a_symbol_to_chip of symbol_to_chip is
 										   "01100000011101111011100011001001",
 										   "10010110000001110111101110001100",
 										   "11001001011000000111011110111000"
-										  ); 
-	signal reg: std_logic_vector(31 downto 0);
-	signal reg_i,reg_q : std_logic_vector(15 downto 0);
+										  );
 	begin
-	shifter_reg:process(clk,reset)
+	shifter_reg: process(clk,reset)
+	  variable reg: std_logic_vector (31 downto 0);
 		begin
 		if clk'event and clk ='1' then
-			if reset ='1' then reg <= (others => '0');
-			elsif Fs='1' then 
-				case symbol IS 
-					when "0000" => reg<= code(0);
-					when "1000" => reg<= code(1);
-					when "0100" => reg<= code(2);
-					when "1100" => reg<= code(3);
-					when "0010" => reg<= code(4);
-					when "1010" => reg<= code(5);
-					when "0110" => reg<= code(6);
-					when "1110" => reg<= code(7);
-					when "0001" => reg<= code(8);
-					when "1001" => reg<= code(9);
-					when "0101" => reg<= code(10);
-					when "1101" => reg<= code(11);
-					when "0011" => reg<= code(12);
-					when "1011" => reg<= code(13);
-					when "0111" => reg<= code(14);
-					when others => reg<= code(15);
-				end case;
-				reg_i <= (reg(1),reg(3),reg(5),reg(7),reg(9),reg(11),reg(13),reg(15),reg(17),
-							reg(19),reg(21),reg(23),reg(25),reg(27),reg(29),reg(31));
-				
-				reg_q <= (reg(0),reg(2),reg(4),reg(6),reg(8),reg(10),reg(12),reg(14),reg(16),
-							reg(18),reg(20),reg(22),reg(24),reg(26),reg(28),reg(30));
+			if reset ='1' then
+			  reg   := (others => '0');
+			elsif Fs='1' then
+			  reg := code(conv_integer(symbol));
 			end if;
 			if Fc='1' then
-				ich0 <= reg_i(15);
-				qch0 <= reg_q(15);
-				reg_i(15 downto 1) <= reg_i(14 downto 0);
-				reg_q(15 downto 1) <= reg_q(14 downto 0);
+				ich0 <= reg(31);
+				qch0 <= reg(30);
+				reg  := reg(29 downto 0) & "00";
 			end if;										
 		end if;
 	end process;                                    
